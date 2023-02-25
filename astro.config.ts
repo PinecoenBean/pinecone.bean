c3e880e2ccb5aa4e60faa8c1fb5fec69cc6a059f
liao <284983325@qq.com>
2023-02-26 02:26:57 +0900

    Initialize the repository

---
+++ astro.config.ts
@@ -0,0 +1,18 @@
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://pinecone.bean.me',
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    mdx(),
+    react(),
+  ],
+})
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface FrontMatter {
+  tags: string[]
+  title: string
+  pubDate: string
+  description: string
+  author: string
+  image: {
+    url: string
+    alt: string
+  }
+}
